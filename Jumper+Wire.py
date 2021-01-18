41626bb54d3eeb3a4a4a3404b12c9cfd98b46ce6
Tobias Schaich <tobias_schaich@hotmail.de>
2021-01-18 14:17:27 +0000

    Calculations with wire and jumper

--- Jumper+Wire.py
+++ Jumper+Wire.py
@@ -15,7 +15,7 @@ import random
 import scipy.io as sio
 
 if __name__=='__main__':    
-    d=1.08*1e-3
+    d=1*1e-2#1.08*1e-3
     cable_length=0.03
     twistlength=0.03
     # random.seed(0)
@@ -23,7 +23,7 @@ if __name__=='__main__':
     # while sum(twistlength) < cable_length:
     #     twistlength.append(0.065+(0.065*0.3*random.uniform(-1,1))) 
     # twistlength=tuple(twistlength)
-    freq=np.linspace(2*1e9,7e9, 901)
+    freq=np.linspace(1*1e9,10e9, 901)
     eps_r=1
     loss_t=0    
     em=em_env.EmEnv(freq,eps_rel=eps_r, loss_tan=loss_t) #set em env
@@ -33,8 +33,8 @@ if __name__=='__main__':
     geo.set_length(cable_length)
     # define wires
     wir1=geometry.CoatedWire((0,-d),  ed_coating = 3, loss_tan = 0.01)
-    wir2=geometry.CoatedWire((0,0),  ed_coating = 3, loss_tan = 0.01)
-    wir3=geometry.CoatedWire((0,1*1e-2),  ed_coating = 3, loss_tan = 00.01)
+    wir2=geometry.CoatedWire((-0.54*1e-3,0),  ed_coating = 3, loss_tan = 0.01)
+    wir3=geometry.CoatedWire((0.54*1e-3,0),  ed_coating = 3, loss_tan = 0.01)
     pair=geometry.TwistedPair(wir2,wir3,twistlength, cable_length)
     # add wires
     geo.add_wire(wir1)
@@ -49,7 +49,7 @@ if __name__=='__main__':
     abcd=sim.run()
     abcd_orig=abcd
     sio.savemat('Jumper_Wire.mat', {'f': freq, 'abcd': abcd_orig})
-    abcd=np.linalg.matrix_power(abcd_orig, 15)
+    abcd=np.linalg.matrix_power(abcd_orig, 100)
     
     
     s=sparameter.Sparameter()
@@ -64,7 +64,7 @@ if __name__=='__main__':
     
     #s.plot()
     #z=np.linspace(1400,1800)
-    z=[900]
+    z=[500]
     for imp in z:
         ntw = rf.Network(frequency=freq, s=s_sw)
         ntw.renormalize(imp)
